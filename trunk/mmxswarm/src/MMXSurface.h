d5123fed4af162e6806027274ad10913938a36d9
lucas.uchida@gmail.com <lucas.uchida@gmail.com@17465e45-e93c-4e35-3779-8888240ace43>
2011-12-09 00:12:07 +0000

    MMX do grupo 15

--- trunk/mmxswarm/src/MMXSurface.h
+++ trunk/mmxswarm/src/MMXSurface.h
@@ -22,7 +22,7 @@ public:
 	virtual void Posterize();	// Grupo 9
 	virtual void GrayFilter();  // Grupo 12
 	//virtual void RGBAdjust();	//Grupo 14
-	//virtual void Mask();		// Grupo 15
+	virtual void Mask();		// Grupo 15
     virtual void Threshold();    //grupo 13
     virtual void Invert();   //GRUPO 7
 	virtual void Solarize();	// Grupo 18
@@ -39,7 +39,6 @@ public:
     virtual void BlurBits(); // this is where the MMX action is	
 	virtual void Sobel();	//Grupo 5
 	virtual void Posterize();	// Grupo 9
-	//virtual void Mask();	//Grupo 15
 protected:
 	virtual void OnCreated();
 private:
@@ -55,7 +54,6 @@ public:
     virtual void BlurBits(); // this is where the MMX action is	
 	virtual void Sobel();	// Grupo 5	
 	virtual void Posterize();	// Grupo 9
-	//virtual void Mask();	// Grupo 15
 protected:
 	virtual void OnCreated();
 private:
