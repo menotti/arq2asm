319a5e73c7ae63344b0fc5800c801e332718e343
menotti@gmail.com <menotti@gmail.com@17465e45-e93c-4e35-3779-8888240ace43>
2011-12-12 11:36:16 +0000

    Mudando o arquivo resource.h para a pasta res, pois o editor de menus gera este arquivo lá se não estiver. O arquivo deve ser removido do projeto e incluido novamente. Atenção para não manter o arquivo nas duas pastas.

---
+++ mmxswarm/res/resource.h
@@ -0,0 +1,77 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by MMXSwarm.rc
+//
+#define IDD_ABOUTBOX                    100
+#define IDR_MAINFRAME                   128
+#define IDR_MMXSwaTYPE                  129
+#define IDD_CIMAGE_16BIT                140
+#define IDD_CIMAGE_24BIT                141
+#define IDD_CIMAGE_32BIT                142
+#define IDD_16BIT_MMXINTRINSICS         150
+#define IDD_24BIT_MMXINTRINSICS         151
+#define IDD_32BIT_MMXINTRINSICS         152
+#define IDD_16BIT_SSE2INTRINSICS        153
+#define IDD_24BIT_SSE2INTRINSICS        154
+#define IDD_32BIT_SSE2INTRINSICS        155
+#define IDD_16BIT_GENERICCBLUR          156
+#define IDD_24BIT_GENERICCBLUR          157
+#define IDD_32BIT_GENERICCBLUR          158
+#define IDS_ERROR_GETEXPORTERFILTER     170
+#define IDS_ERROR_PROPERTYFRAME         171
+#define IDS_ERROR_GETPAGES              172
+#define IDS_WARNING_BITDEPTH            173
+#define IDS_WARNING_DEBUG               174
+#define IDS_ERROR_SAVE                  175
+#define IDS_ALL_IMAGES                  176
+#define ID_STATIC_MODE                  177
+#define ID_MODE_WEBCAM                  178
+#define ID_INDICATOR_FPS                59142
+#define ID_INDICATOR_RESOLUTION         59143
+#define ID_INDICATOR_PAUSE_SWARM        59144
+#define ID_INDICATOR_PAUSE_BLUR         59145
+#define ID_INDICATOR_PAUSE_BLIT         59146
+#define ID_INDICATOR_USE_FADE           59147
+#define ID_INDICATOR_USE_GRAY           59148
+#define ID_INDICATOR_USE_SOBEL          59149
+#define ID_INDICATOR_USE_POSTERIZE      59150
+#define ID_INDICATOR_USE_GRAYF          59151
+#define ID_INDICATOR_USE_RGBADJUST      59152
+#define ID_INDICATOR_USE_MASK           59153
+#define ID_INDICATOR_USE_MANDEL         59154
+#define ID_INDICATOR_USE_SOLARIZE       59155
+#define ID_INDICATOR_USE_THRESHOLD      59156
+#define ID_INDICATOR_USE_INVERT         59157
+#define ID_INDICATOR_USE_WEBCAM         59158
+#define ID_INDICATOR_USE_GRADIENT       59159
+#define ID_INDICATOR_USE_CHANNELMIX     59160
+#define ID_VIEW_PAUSE_SWARM             59394
+#define ID_VIEW_PAUSE_BLUR              59395
+#define ID_VIEW_PAUSE_BLIT              59396
+#define ID_VIEW_USE_FADE                59397
+#define ID_VIEW_USE_GRAY                59398
+#define ID_VIEW_USE_SOBEL               59399
+#define ID_VIEW_USE_POSTERIZE           59400
+#define ID_VIEW_USE_GRAYF               59401
+#define ID_VIEW_USE_RGBADJUST           59402
+#define ID_VIEW_USE_MASK                59403
+#define ID_VIEW_USE_MANDEL              59404
+#define ID_VIEW_USE_SOLARIZE            59405
+#define ID_VIEW_USE_THRESHOLD           59406
+#define ID_VIEW_USE_INVERT              59407
+#define ID_VIEW_USE_GRADIENT            59408
+#define ID_VIEW_USE_CHANNELMIX          59409
+#define ID_MODE_IMAGE                   59410
+#define ID_MODE_FADE                    59411
+#define ID_MODE_SWARM                   59412
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        177
+#define _APS_NEXT_COMMAND_VALUE         59413
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
