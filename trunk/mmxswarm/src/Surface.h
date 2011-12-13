a5994526e5b911154f896ad7cc8d0ee2f24085d5
menotti@gmail.com <menotti@gmail.com@17465e45-e93c-4e35-3779-8888240ace43>
2011-12-13 21:08:04 +0000

    Desfazendo revisao 215

--- trunk/mmxswarm/src/Surface.h
+++ trunk/mmxswarm/src/Surface.h
@@ -80,9 +80,6 @@ public:
 
 	 //Grupo 11
 	 virtual void ChannelMix();
-	 
-	 //Grupo 20
-	 virtual void Dither();
 
 	 //Grupo 5
 	 virtual void Sobel(); // neste método está a implementação do Sobel
