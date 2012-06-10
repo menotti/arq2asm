2c67a80c0bb263521bfdd1735b4cdc180b3cb33f
menotti@gmail.com <menotti@gmail.com@17465e45-e93c-4e35-3779-8888240ace43>
2012-06-10 19:50:47 +0000

    Paths updated

--- mmxswarm/src/MMXSwarm.h
+++ mmxswarm/src/MMXSwarm.h
@@ -15,7 +15,7 @@
 #error include 'stdafx.h' before including this file for PCH
 #endif
 
-#include "..\res\resource.h"       // main symbols
+#include "resource.h"       // main symbols
 
 /////////////////////////////////////////////////////////////////////////////
 // CMMXSwarmApp:
