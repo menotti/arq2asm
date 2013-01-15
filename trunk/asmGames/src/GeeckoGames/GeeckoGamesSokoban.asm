dafc4e2ce0c39ee33acd03fc33b6aae1f506d9e2
geeckoGames@gmail.com <geeckoGames@gmail.com@17465e45-e93c-4e35-3779-8888240ace43>
2013-01-15 00:27:51 +0000

    Adicionado um LoadMap e um DrawMap, outras coisas já estão pré-prontas, mas ainda não integradas ao código final

--- trunk/asmGames/src/GeeckoGames/GeeckoGamesSokoban.asm
+++ trunk/asmGames/src/GeeckoGames/GeeckoGamesSokoban.asm
@@ -1,13 +1,50 @@
-TITLE GeeckoGamesSokoban				
+LoadMap PROTO, destinationMap : PTR BYTE
 
 .data
-
+mapFileName BYTE "C:\\Irvine\\Examples\\ProjectoFinalArq2\\TestSokobanMap.map", 0
+currentMap BYTE 438 DUP (?)
 .code
 
 GeeckoGamesSokoban PROC
-
 menu:
+	pusha
 	call ClrScr
+	mov EDX, OFFSET mapFileName
+
+	INVOKE LoadMap, ADDR currentMap
+
+	mov ESI, OFFSET currentMap
+	mov ECX, 438
+
+	call DrawMap
 
 	call WaitMsg
+	popa
+	ret
 GeeckoGamesSokoban ENDP
+
+;RECEIVES THE ADDRESS OF THE MAP IN ESI AND THE NUMBER OF CHARS IN IT IN ECX
+DrawMap PROC USES EAX EBX
+L:	mov al, BYTE PTR [esi]
+	call WriteChar
+	inc ESI
+L2:	loop L
+	call Crlf
+	ret 
+DrawMap ENDP
+
+;THIS FUNCTION TAKES A FILENAME IN EDX AND LOADS AN ARRAY
+;IN THE ADDRESS PASSED AS AN ARGUMENT
+LoadMap PROC USES EAX ECX, destinationMap : PTR BYTE
+	call OpenInputFile
+	push eax
+
+	mov EDX, destinationMap
+	mov ECX, 438
+	call ReadFromFile
+
+	pop eax
+	
+	call CloseFile
+	ret
+LoadMap ENDP
