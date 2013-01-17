12e859ee17e345fc70d485e16cfb2001731d2c07
geeckoGames <geeckoGames@17465e45-e93c-4e35-3779-8888240ace43>
2013-01-17 16:44:59 +0000

    Implementação da função de desenhar o background

--- trunk/asmGames/src/GeeckoGames/GeeckoGamesDrawing.asm
+++ trunk/asmGames/src/GeeckoGames/GeeckoGamesDrawing.asm
@@ -1,6 +1,6 @@
 
-DrawBackground PROTO
-DrawInteractive PROTO
+DrawBackground PROTO, mapPtr:PTR BYTE, lineSize:BYTE, mapSize:DWORD
+DrawInteractive PROTO, mapPtr:PTR BYTE, lineSize:BYTE, mapSize:DWORD
 DrawMainScreen PROTO
 DrawFinishedGame PROTO
 
@@ -9,11 +9,25 @@ DrawFinishedGame PROTO
 
 .code
 
-DrawBackground PROC
-	RET
+DrawBackground PROC, mapPtr:PTR BYTE, lineSize:BYTE, mapSize:DWORD
+	MOV ECX, mapSize
+	MOV DX, 0
+	MOV ESI, mapPtr	
+WriteC:
+	MOV EAX, [ESI]
+	CALL WriteChar
+	INC ESI
+	INC DL
+	CMP DL, lineSize
+	JL NextC
+	MOV DL, 0	
+	INC DH		;new line
+NextC:
+	LOOP WriteC
+RET
 DrawBackground ENDP
 
-DrawInteractive PROC	
+DrawInteractive PROC, mapPtr:PTR BYTE, lineSize:BYTE, mapSize:DWORD	
 	RET
 DrawInteractive ENDP
 
